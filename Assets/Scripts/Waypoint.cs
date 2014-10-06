0497936b8d7264c06ddff8476d6635557f782582
unknown <rml8300@IGMGRAD-12.main.ad.rit.edu>
2014-10-05 21:23:00 -0400

    Kill files

--- Assets/Scripts/Waypoint.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Waypoint : MonoBehaviour {
-	public bool isStart = false;
-	public Waypoint loopBackTo;
-	public int maxLoopBacks;
-	public int loopBacks;
-}
